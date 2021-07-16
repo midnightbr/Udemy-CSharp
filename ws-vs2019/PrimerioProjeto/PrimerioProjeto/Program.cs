75567e66f4d6245ce98f66af6ab3e3d7c0a05c0e
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-07-16 10:09:54 -0300

    Olá Mundo
    Inicio do curso de C#.
    Olá mundo em C# com o workspace no Visual Studio 2019

---
+++ ws-vs2019/PrimerioProjeto/PrimerioProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimerioProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+        }
+    }
+}
