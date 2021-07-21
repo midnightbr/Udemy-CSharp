aaa9f1979da6b5182db82657763c8568e282aa64
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-07-20 21:11:26 -0300

    Métodos de saída de dados
    Nessa aula foi mostrado os três tipos de métodos que podem ser usadas para saída de dados, que são: Placeholders, Concatencção e Interpolação.

---
+++ ws-rider/Concatenacao_Placeholders_Interpolacao/Concatenacao_Placeholders_Interpolacao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Concatenacao_Placeholders_Interpolacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Clayton";
+            int years = 34;
+            double balance = 556.67548;
+            
+            // Método Placeholders
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a R${2:F2} reais", name, years, balance);
+            
+            // Interpolação
+            Console.WriteLine($"{name} tem {years} anos e tem saldo igual a R${balance:F2} reais.");
+            
+            // Concatenação
+            Console.WriteLine(name + " tem " + years + " anos e tem saldo igual a R$" + balance.ToString("F2") + " reais");
+        }
+    }
+}
