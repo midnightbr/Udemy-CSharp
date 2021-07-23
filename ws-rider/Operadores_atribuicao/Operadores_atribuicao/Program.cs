bc2811f9db0e7fa29ee7a063452d7b742fbcb3a4
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-07-22 23:22:10 -0300

    Operadores de atribuição
    Visto algumas formas de poder atribuir um valor a uma variável. Uma delas é poder somar o valor da variável com mais algum outro valor, sem precisar de ter uma expressão muito grande. Exemplo:
    A =+ 2

---
+++ ws-rider/Operadores_atribuicao/Operadores_atribuicao/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Operadores_atribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarando uma variavel com um valor do tipo inteiro
+            int a = 10;
+            Console.WriteLine(a);
+            
+            // A variavel 'a' recebe o valor dela com uma adição do valor 2
+            a += 2;
+            Console.WriteLine(a);
+            
+            // A variavel 'a' recebe o valor dela com uma subtração do valor 2
+            a -= 2;
+            Console.WriteLine(a);
+            
+            // A variavel 'a' recebe o valor dela com uma multiplicação por 2
+            a *= 2;
+            Console.WriteLine(a);
+            
+            // A variavel 'a' recebe o valor dela com uma divisão por 2
+            a /= 2;
+            Console.WriteLine(a);
+            
+            // A variavel 'a' recebe o valor do resto da divisão do valor dela por 3
+            a %= 3;
+            Console.WriteLine(a);
+        }
+    }
+}
