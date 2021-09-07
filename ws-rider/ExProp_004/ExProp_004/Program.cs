73d390f0859f1a1e9e753d8799e7ce45961a32a9
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-09-07 16:16:40 -0300

    Exercício Propostos 004
    Programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.

---
+++ ws-rider/ExProp_004/ExProp_004/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProp_004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hour, number, value, salary;
+
+            Console.Write("Digite o número do funcionario: ");
+            number = double.Parse(Console.ReadLine());
+            Console.Write("Digite as horas trabalhadas: ");
+            hour = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor por hora trabalhada: ");
+            value = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salary = value * hour;
+
+            Console.WriteLine("Number: " + number);
+            Console.WriteLine("Salary: U$" + salary.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
