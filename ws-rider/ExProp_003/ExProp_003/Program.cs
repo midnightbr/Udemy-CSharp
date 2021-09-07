01c141f3a055287cd289032a27f3e4df1376e971
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-09-07 16:15:09 -0300

    Exercício Propostos 003
    programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D.

---
+++ ws-rider/ExProp_003/ExProp_003/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExProp_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, dif;
+            Console.WriteLine("Digite apenas números inteiros!");
+            Console.Write("Número A: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Número B: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Número C: ");
+            c = int.Parse(Console.ReadLine());
+            Console.Write("Número D: ");
+            d = int.Parse(Console.ReadLine());
+
+            dif = (a * b) - (c * d);
+            
+            Console.WriteLine("O total do produto entre A e B é " + (a * b));
+            Console.WriteLine("O total do produto entre C e D é " + (c * d));
+            Console.WriteLine("E a diferença entre A * B de C * D é " + dif);
+        }
+    }
+}
