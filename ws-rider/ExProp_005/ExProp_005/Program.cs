e68ffb12e2fc87dfb7548cf158ed87113e502c38
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-09-07 16:17:59 -0300

    Exercício Propostos 005
    Programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.

---
+++ ws-rider/ExProp_005/ExProp_005/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+
+namespace ExProp_005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cod, quant;
+            double value, tot1, tot2, tot;
+
+            Console.WriteLine("Digite o código, quantidade de peças e o valor unitario separados por espaço: ");
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            cod = int.Parse(vetor[0]);
+            quant = int.Parse(vetor[1]);
+            value = Double.Parse(vetor[2], CultureInfo.InvariantCulture);
+            tot1 = quant * value;
+            
+            Console.WriteLine("Digite o código, quantidade de peças e o valor unitario separados por espaço: ");
+            string[] vetor2 = Console.ReadLine().Split(' ');
+
+            cod = int.Parse(vetor2[0]);
+            quant = int.Parse(vetor2[1]);
+            value = Double.Parse(vetor2[2], CultureInfo.InvariantCulture);
+            tot2 = quant * value;
+
+            tot = tot1 + tot2;
+
+            Console.WriteLine("Valor a pagar: R$" + tot.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
