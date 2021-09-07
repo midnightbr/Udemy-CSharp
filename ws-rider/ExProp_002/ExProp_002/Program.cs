83b4d10f1f565a254f0f480c7ea220b8cd52578c
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-09-07 16:13:57 -0300

    Exercícios Propostos 002
    Programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais.
    Considere o valor de π = 3.14159

---
+++ ws-rider/ExProp_002/ExProp_002/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProp_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, pi, raio;
+
+            pi = 3.14159;
+
+            Console.WriteLine("Calculando a área do circulo!");
+            Console.Write("Digite o valor do raio do circulo: ");
+            raio = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            // Calculando área do circulo
+            area = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("Area do circulo com um raio de " + raio.ToString(CultureInfo.InvariantCulture) +
+                              " é de " + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ws-rider/ExProp_006/ExProp_006/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProp_006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Digite 3 valores separados por espaço:");
+            string[] vetor = Console.ReadLine().Split(' ');
+            a = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+
+            double tria, cir, trap, quad, reta, pi;
+            pi = 3.14159;
+            tria = (a * c) / 2;
+            cir = pi * Math.Pow(c, 2);
+            trap = (a + b) * c / 2;
+            quad = Math.Pow(b, 2);
+            reta = a * b;
+            Console.WriteLine("Triangulo: " + tria.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Circulo: " + cir.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Trapezio: " + trap.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Quadrado: " + quad.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Retangulo: " + reta.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
