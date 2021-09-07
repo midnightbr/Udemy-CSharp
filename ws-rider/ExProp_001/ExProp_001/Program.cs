bfb8c381b51d1b00cbc0faf4e36bfd3e27351d3f
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-09-07 16:12:06 -0300

    Exercício Propostos 001
    Programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa.

---
+++ ws-rider/ExProp_001/ExProp_001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExProp_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number01;
+            int number02;
+            int result;
+
+            Console.WriteLine("Digite apenas números inteiros!");
+            Console.Write("Primeiro número: ");
+            number01 = int.Parse(Console.ReadLine());
+            Console.Write("Segundo número: ");
+            number02 = int.Parse(Console.ReadLine());
+
+            result = number01 + number02;
+
+            Console.WriteLine("Resultado da soma: " + result);
+        }
+    }
+}
