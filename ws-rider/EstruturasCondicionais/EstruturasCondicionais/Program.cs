4feee2f6cbe2feddd8ee77beab21e4d43c391771
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-09-13 01:43:03 -0300

    Exercício Proposto 008 Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em ordem crescente ou decrescente.

---
+++ ws-rider/EstruturasCondicionais/EstruturasCondicionais/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EstruturasCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Primeiro exemplo de condição (if-else)
+            Console.Write("Entre com um número inteiro: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("O número digitado é par.");
+            }
+            else {
+                Console.WriteLine("O número digitado é impar.");
+            }
+            
+            // Segundo exemplo de condição (if-else-else if)
+            Console.Write("Qual a hora atual? ");
+            int hour = int.Parse(Console.ReadLine());
+            if (hour >= 6 && hour < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hour < 19)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else if (hour < 24)
+            {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
---
+++ ws-rider/ExProp_007/ExProp_007/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExProp_007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 0)
+            {
+                Console.WriteLine("O número digitado não é NEGATIVO.");
+            }
+            else
+            {
+                Console.WriteLine("O número digitado é NEGATIVO.");
+            }
+        }
+    }
+}
