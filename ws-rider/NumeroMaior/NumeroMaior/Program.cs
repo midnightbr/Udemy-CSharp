850ae91c6e445a45a311937ec476accb3861fa0f
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-16 09:19:32 -0300

    Introdução a Funções
    Aula introdutória sobre funções. A primeira função com retorno de um valor inteiro. Além de dar uma olhada na questão de debugger.

---
+++ ws-rider/NumeroMaior/NumeroMaior/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NumeroMaior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite três números: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine()); 
+
+            int resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("O maior número digitado foi: " + resultado);
+            // F8 debugger passo a passo
+            // F7 entrada dentro da função no debugger
+        }
+
+        static int Maior(int a, int b, int c)
+        {
+            int maior;
+            if(a > b && a > c)
+            {
+                maior = a;
+            }
+            else if (b > c)
+            {
+                maior = b;
+            }
+            else
+            {
+                maior = c;
+            }
+
+            return maior;
+        }
+    }
+}
