d1d306ab2cc2a5c6ac39bf9e985642fe3fb62be5
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-16 09:42:28 -0300

    Exercício Proposto 10
    Nesse exercício, foi proposto um programa que informa-se a localização do objeto com as coordenadas x e y fornecidas pela o usuário. Para poder testar o conhecimento obtido nas aulas, usei uma função para tal, junto com as condições de verificações.

---
+++ ws-rider/ExProp_10/ExProp_10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ExProp_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a coordenada X: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Digite a coordenada Y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(quadrante(x, y));
+
+        }
+
+        static string quadrante(double a, double b)
+        {
+            string res = "";
+            
+            if (a > 0 && b > 0)
+            {
+                res = "Primeiro";
+            }
+            else if (a > 0 && b < 0)
+            {
+                res = "Quarto";
+            }
+            else if (a < 0 && b < 0)
+            {
+                res = "Terceiro";
+            }
+            else if (a < 0 && b > 0)
+            {
+                res = "Segundo";
+            }
+
+            return res;
+        }
+    }
+}
