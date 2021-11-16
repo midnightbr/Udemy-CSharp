9487de03f569862c1b7254b47204ce4ca9afa59d
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-16 09:14:45 -0300

    Estrutura While
    Aula explicando sobre a estrutura while.

---
+++ ws-rider/EstruturaWhile/EstruturaWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (number >= 0.0)
+            {
+                double raiz = Math.Sqrt(number);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                
+                Console.Write("Digite um número: ");
+                number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            
+            Console.WriteLine("Número negativo");
+            
+        }
+    }
+}
