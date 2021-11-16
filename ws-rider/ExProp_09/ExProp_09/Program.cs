797d043e8b38d2bdc2e6a83db3d322a35608dc2a
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-16 09:35:34 -0300

    Exercício Proposto 09
    O exercício proposto e um programa simples, no qual testa a condição while e suas respectivas condições. Enquanto a senha digitada não for 2002, o programa informara "Senha invalida" ao usuário.

---
+++ ws-rider/ExProp_09/ExProp_09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExProp_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                Console.Write("Digite a senha: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
