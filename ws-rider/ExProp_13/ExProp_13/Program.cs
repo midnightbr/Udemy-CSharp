c8b31045972e50534d0a91a99bf852a47add429f
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-20 11:02:12 -0300

    Exercício Proposto 13
    O programa devera perguntar quantos valores o usuário digitara, no qual o programa devera informar quantos valores foram digitados entre os números 10 a 20 e quantos foram digitados foram desse intervalo.

---
+++ ws-rider/ExProp_13/ExProp_13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExProp_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros ira digitar? ");
+            int num = int.Parse(Console.ReadLine());
+            
+            faixa(num);
+            
+        }
+
+        static void faixa(int num)
+        {
+            int IN = 0;
+            int OUT = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write("Digite um número: ");
+                int number = int.Parse(Console.ReadLine());
+                if (number >= 10 && number <= 20)
+                {
+                    IN += 1;
+                }
+                else if (number <= 10 || number >= 20)
+                {
+                    OUT += 1;
+                }
+            }
+
+            Console.WriteLine("IN: {0}", IN);
+            Console.WriteLine("OUT: {0}", OUT);
+        }
+    }
+}
