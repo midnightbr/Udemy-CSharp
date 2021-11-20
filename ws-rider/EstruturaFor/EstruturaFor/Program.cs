10185901e4d860399166a2e66b9bb68b5689f54b
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-20 10:48:00 -0300

    Estrutura For
    Aula explicando sobre os detalhes da funcionalidade da estrutura for.

---
+++ ws-rider/EstruturaFor/EstruturaFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstruturaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você ira digitar? ");
+            int number = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+
+                soma += valor;
+            }
+
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
