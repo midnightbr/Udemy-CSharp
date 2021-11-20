4109f69f51f8dad9592a30d9359a3b9f2e2d20d8
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-20 11:20:37 -0300

    Exercício Proposto 16
    Ler um valor N. Calcular e escrever seu respectivo fatorial.

---
+++ ws-rider/ExProp_16/ExProp_16/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExProp_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do fatorial: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int fat = 1;
+
+            for (int i = 0; i < num; i++)
+            {
+                fat = (num - i) * fat;
+            }
+
+            Console.WriteLine("O fatorial de {0} é {1}", num, fat);
+        }
+    }
+}
