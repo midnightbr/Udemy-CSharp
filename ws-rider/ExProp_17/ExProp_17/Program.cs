3d461f6e02d1fcbfdc2fdddeb2854fcb3c87bc28
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-20 11:25:59 -0300

    Exercício Proposto 17
    Ler um número inteiro N e calcular todos os seus divisores.

---
+++ ws-rider/ExProp_17/ExProp_17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExProp_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= num; i++)
+            {
+                int div = num % i;
+
+                if (div == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
