05c6a6a24c7b715033ff6629c454be5fa5f41a93
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-20 11:16:15 -0300

    Exercício Proposto 15
    Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
    segundo. Se o denominador for igual a zero, mostrar a mensagem "divisão impossível".

---
+++ ws-rider/ExProp_15/ExProp_15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExProp_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantidade de testes: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                double div = 0.0;
+                string[] line = Console.ReadLine().Split(' ');
+                double a = Double.Parse(line[0]);
+                double b = Double.Parse(line[1]);
+
+                if (a == 0 || b == 0)
+                {
+                    Console.WriteLine("Divisão impossivel");
+                }
+                else
+                {
+                    div = a / b;
+                    Console.WriteLine(div);
+                }
+
+            }
+        }
+    }
+}
