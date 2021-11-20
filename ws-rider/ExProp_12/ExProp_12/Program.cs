73398ba35d738d6a1df7a53e5e22e0c31752c89d
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-20 10:59:40 -0300

    Exercício Proposto 12
    Exercício no qual foi pedido para que o programa leia números até o número que o usuário digitou e mostrar quais são impares.

---
+++ ws-rider/ExProp_12/ExProp_12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExProp_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Até qual número deseja saber quais são os números impares? ");
+            int num = int.Parse(Console.ReadLine());
+            
+            impar(num);
+        }
+
+        static void impar(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
