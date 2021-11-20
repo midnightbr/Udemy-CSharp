d786d584093cc40603bf15e91aebef5b49bbc352
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-20 11:10:26 -0300

    Exercício Proposto 14
    Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
    de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
    conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
    peso 5.

---
+++ ws-rider/ExProp_14/ExProp_14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExProp_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos casos de testes? ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                double a = Double.Parse(line[0]);
+                double b = Double.Parse(line[1]);
+                double c = Double.Parse(line[2]);
+
+                double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10;
+
+                Console.WriteLine(media.ToString("F1"));
+            }
+        }
+    }
+}
