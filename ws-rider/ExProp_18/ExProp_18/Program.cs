826984e2959905c35331ed26387a387ff0277f29
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-20 11:30:08 -0300

    Exercício Proposto 18
    Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
    começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor.

---
+++ ws-rider/ExProp_18/ExProp_18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExProp_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor inteiro positivo: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                int quad = (int)Math.Pow(i, 2);
+                int trip = (int)Math.Pow(i, 3);
+                
+                Console.Write(i + " ");
+                Console.Write(quad + " ");
+                Console.WriteLine(trip);
+            }
+        }
+    }
+}
