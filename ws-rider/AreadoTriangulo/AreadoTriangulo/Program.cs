52af3aae28ad549df778d4d9626245fcbaef9f23
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-21 23:26:15 -0300

    Classes, Objetos e Atributos
    Nessa aula, começamos a alterar o código do programa sobre triângulos, adaptando e dando iniciativa na solução do problema com Orientação a Objetos (OO). Primeiro criamos uma nova classe chamada triângulo, no qual passou a receber os dados inseridos pelo o usuário, sem precisar declarar várias variáveis (xA, xB, xC, yA, yB, yC) no programa principal. Além de instancia a nova classe no programa principal.

--- ws-rider/AreadoTriangulo/AreadoTriangulo/Program.cs
+++ ws-rider/AreadoTriangulo/AreadoTriangulo/Program.cs
@@ -7,27 +7,45 @@ namespace AreadoTriangulo
     {
         static void Main(string[] args)
         {
-            double xA, xB, xC, yA, yB, yC;
+            /**
+             * Metodo antigo
+             * double xA, xB, xC, yA, yB, yC;
+             */
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+            
             Console.WriteLine("Entre com as medidas do triangulo X: ");
             Console.Write("A: ");
-            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
             Console.Write("B: ");
-            xB = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //xB = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
             Console.Write("C: ");
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
             Console.WriteLine("Entre com as medidas do triangulo Y: ");
             Console.Write("A: ");
-            yA = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            // yA = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
             Console.Write("B: ");
-            yB = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            // yB = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
             Console.Write("C: ");
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            double pX = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(pX * (pX - x.A) * (pX - x.B) * (pX - x.C));
 
-            double pX = (xA + xB + xC) / 2.0;
-            double areaX = Math.Sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC));
-
-            double pY = (yA + yB + yC) / 2.0;
-            double areaY = Math.Sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+            double pY = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(pY * (pY - y.A) * (pY - y.B) * (pY - y.C));
 
             Console.WriteLine("Área de X = {0}", areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = {0}", areaY.ToString("F4", CultureInfo.InvariantCulture));
---
+++ ws-rider/AreadoTriangulo/AreadoTriangulo/Triangulo.cs
@@ -0,0 +1,8 @@
+namespace AreadoTriangulo {
+    public class Triangulo {
+        // Nome de atributo começa com a letra Maiuscula
+        public double A;
+        public double B;
+        public double C;
+    }
+}
