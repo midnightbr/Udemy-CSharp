a3a727dab646e4f00818b176ded9b0e9d67f2337
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-21 22:20:34 -0300

    Triângulos - Sem OO
    Solucionando um problema no qual, independente do tipo de triângulo escolhido, o programa calcule a área dos dois triângulos e informa qual deles tem uma maior área.
    Essa solução não usa Orientação a Objetos (OO).

---
+++ ws-rider/AreadoTriangulo/AreadoTriangulo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreadoTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Entre com as medidas do triangulo X: ");
+            Console.Write("A: ");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("B: ");
+            xB = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("C: ");
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triangulo Y: ");
+            Console.Write("A: ");
+            yA = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("B: ");
+            yB = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("C: ");
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pX = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC));
+
+            double pY = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+
+            Console.WriteLine("Área de X = {0}", areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = {0}", areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
