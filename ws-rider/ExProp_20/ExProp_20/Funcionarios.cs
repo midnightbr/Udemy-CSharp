f8dc11d471d1bec22067526afd6a571237177485
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-23 17:27:22 -0300

    Exercício Proposto 20
    Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.

---
+++ ws-rider/ExProp_20/ExProp_20/Funcionarios.cs
@@ -0,0 +1,6 @@
+namespace ExProp_20 {
+    public class Funcionarios {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ ws-rider/ExProp_20/ExProp_20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProp_20 {
+    class Program {
+        static void Main(string[] args) {
+            Funcionarios func1 = new Funcionarios();
+            Funcionarios func2 = new Funcionarios();
+            
+            // Coletando dados do primeiro funcionario
+            Console.WriteLine("Dados do primeiro funcionario:");
+            Console.Write("Nome: ");
+            func1.nome = Console.ReadLine();
+            Console.Write("Salário: R$");
+            func1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            // Coletando dados do segundo funcionario
+            Console.WriteLine("Dados do segundo funcionario:");
+            Console.Write("Nome: ");
+            func2.nome = Console.ReadLine();
+            Console.Write("Salário: R$");
+            func2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (func1.salario + func2.salario) / 2;
+
+            Console.WriteLine("Salário médio: R${0}", media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
