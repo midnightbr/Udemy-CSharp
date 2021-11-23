3d9ecadbf93ee1f87374b6787401dc3b10d29db0
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-23 16:10:00 -0300

    Exercício Proposto 19
    Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha, usando o método de classes para coletar os dados.

---
+++ ws-rider/ExProp_19/ExProp_19/Pessoas.cs
@@ -0,0 +1,6 @@
+namespace ExProp_19 {
+    public class Pessoas {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ ws-rider/ExProp_19/ExProp_19/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExProp_19 {
+    class Program {
+        static void Main(string[] args) {
+            Pessoas one = new Pessoas();
+            Pessoas two = new Pessoas();
+            
+            // Recebendo dados do usuário sobre a primeira pessoa
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            one.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            one.idade = int.Parse(Console.ReadLine());
+            
+            // Recebendo dados do usuário sobre a segunda pessoa
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            two.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            two.idade = int.Parse(Console.ReadLine());
+
+            if (one.idade > two.idade) {
+                Console.WriteLine("Pessoa mais velha: {0}", one.nome);
+            }
+            else {
+                Console.WriteLine("Pessoa mais velha: {0}", two.nome);
+            }
+
+        }
+    }
+}
