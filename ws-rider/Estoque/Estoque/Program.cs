708b5d14ff8775ea2159b4ed5f5fce2cec605083
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-11-30 13:53:22 -0300

    Controle de Estoque
    Programa feito para demonstrar a usabilidade das classes e métodos, assim como utilizar métodos de um classe externa na classe principal.

---
+++ ws-rider/Estoque/Estoque/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque {
+    class Program {
+        static void Main(string[] args) {
+            Produto p = new Produto();
+            
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: R$");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: {0}", p);
+            Console.WriteLine();
+            
+            Console.Write("Digite o número de produtos a serem adicionados: ");
+            int qte = int.Parse(Console.ReadLine());
+            
+            p.AdicionarProdutos(qte);
+            
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: {0}", p);
+            Console.WriteLine();
+            
+            Console.Write("Digite o número de produtos a serem deletados: ");
+            qte = int.Parse(Console.ReadLine());
+            
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: {0}", p);
+        }
+    }
+}
