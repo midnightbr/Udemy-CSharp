098f600345f12722752bc9936505d9b835c519c5
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2021-12-28 20:37:52 -0300

    Introdução a Encapsulamento O programa foi alterado para poder dar inicio aos estudos de encapsulamento. De como é feito e o porque, além de mostrar como acessar aos parametros que estão privados para a classe principal, assim como métodos para poder alterar os mesmos. Usamos os métodos Get e Set nessa aula.

--- ws-rider/Estoque/Estoque/Produto.cs
+++ ws-rider/Estoque/Estoque/Produto.cs
@@ -3,38 +3,68 @@ using System.Globalization;
 
 namespace Estoque {
     public class Produto {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        
+        /**
+         * Por conversão da linguagem C#, quando se trabalha com atributos privativos,
+         * deve-se nomea-los com _ seguinto por letras minusculas, como os
+         * exemplos abaixo.
+         */
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         public Produto(string nome, double preco) {
-            Nome = nome;
-            Preco = preco;
+            _nome = nome;
+            _preco = preco;
         }
         
         /* Alterando o código para reaproveitar o construtor anterior, fazendo assim
          * com que não tenha tanta repetição no código, fazendo ele ficar mais limpo e eficaz.
          */
         public Produto(string nome, double preco, int quantidade) : this(nome, preco) {
-            Quantidade = quantidade;
+            _quantidade = quantidade;
         }
 
+        public string GetNome() {
+            return _nome;
+        }
+
+        public void SetNome(string nome) {
+            /**
+             * Com o encapsulamento, pode-se adicionar as regras do negocio dentro
+             * dos métodos, como no exemplo abaixo.
+             * Na condicional abaixo, ele verficia se a variavel não está vazia e que tenha
+             * mais que 1 caracter na frase, para depois poder ser alterado.
+             */
+            if (nome != null && nome.Length > 1) {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco() {
+            return _preco;
+        }
+
+        public int GetQuantidade() {
+            return _quantidade;
+        }
+        
         public double ValorTotalEmEstoque() {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade) {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade) {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString() {
-            return Nome + ", R$" 
-                        + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " 
-                        + Quantidade + " unidades, Total: R$"
+            return _nome + ", R$" 
+                        + _preco.ToString("F2", CultureInfo.InvariantCulture) + ", " 
+                        + _quantidade + " unidades, Total: R$"
                         + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
--- ws-rider/Estoque/Estoque/Program.cs
+++ ws-rider/Estoque/Estoque/Program.cs
@@ -4,39 +4,11 @@ using System.Globalization;
 namespace Estoque {
     class Program {
         static void Main(string[] args) {
-            //Produto p = new Produto();
-            
-            Console.WriteLine("Entre com os dados do produto:");
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preço: R$");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Quantidade: ");
-            int quantidade = int.Parse(Console.ReadLine());
+            Produto p = new Produto("TV", 900.00, 15);
 
-            Produto p = new Produto(nome, preco, quantidade);
-            
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: {0}", p);
-            Console.WriteLine();
-            
-            Console.Write("Digite o número de produtos a serem adicionados: ");
-            int qte = int.Parse(Console.ReadLine());
-            
-            p.AdicionarProdutos(qte);
-            
-            Console.WriteLine();
-            Console.WriteLine("Dados Atualizados: {0}", p);
-            Console.WriteLine();
-            
-            Console.Write("Digite o número de produtos a serem deletados: ");
-            qte = int.Parse(Console.ReadLine());
-            
-            p.RemoverProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados Atualizados: {0}", p);
-            
+            Console.WriteLine("Nome: " + p.GetNome());
+            Console.WriteLine("Preço: R$" + p.GetPreco());
+            Console.WriteLine("Quantidade: " + p.GetQuantidade());
         }
     }
 }
