46345565534525c435b61c780fb999710a66bb35
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-01-02 19:51:29 -0300

    Exercício Proposto 26 A dona de um pensionato possui dez quartos para alugar para estudantes, sendo esses quartos identificados pelos números 0 a 9. Quando um estudante deseja alugar um quarto, deve-se registrar o nome e email deste estudante. Fazer um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes. Para cada registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de todas ocupações do pensionato, por ordem de quarto

---
+++ ws-rider/ExProp_26/ExProp_26/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExProp_26 {
+    class Program {
+        static void Main(string[] args) {
+            
+            Rent[] vector = new Rent[10];
+            
+            Console.Write("How many rooms will be rented? ");
+            int rent = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < rent; i++) {
+                Console.WriteLine("Rent #{0}", (i+1));
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                vector[room] = new Rent { Name = name, Email = email, Room = room };
+                
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0; i < 10; i++) {
+                if (vector[i] != null) {
+                    Console.WriteLine(vector[i].Room + ": " 
+                                                     + vector[i].Name + ", " 
+                                                     + vector[i].Email);
+                }
+            }
+        }
+    }
+}
---
+++ ws-rider/ExProp_26/ExProp_26/Rent.cs
@@ -0,0 +1,7 @@
+namespace ExProp_26 {
+    public class Rent {
+        public string Name { get; set; }
+        public int Room { get; set; }
+        public string Email { get; set; }
+    }
+}
