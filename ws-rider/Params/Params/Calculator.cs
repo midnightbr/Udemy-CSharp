726ade2ce7d447eb3abf545c398313ad07b48275
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-01-05 14:19:26 -0300

    Params Introdução ao conceito params, no qual resolve o problema de criação de um método com infiniteis possibilidades.

---
+++ ws-rider/Params/Params/Calculator.cs
@@ -0,0 +1,12 @@
+namespace Params {
+    public class Calculator {
+        public static int Sum(params int[] numbers) {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
