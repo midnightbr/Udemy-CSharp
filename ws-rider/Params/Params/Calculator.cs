3ac53fee436bb7f9451127d07fcaeea2a3cbb7ea
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-01-05 21:58:58 -0300

    Modificador de parâmetros Aula sobre os modificadores de parâmetros ref e out, no qual foi apresentado o funcionamento de cada um. No entanto, tem uma observação. Não é recomendado a utilização de nenhum dos dois, por ser considerados código ruim.

--- ws-rider/Params/Params/Calculator.cs
+++ ws-rider/Params/Params/Calculator.cs
@@ -5,8 +5,25 @@ namespace Params {
             for (int i = 0; i < numbers.Length; i++) {
                 sum += numbers[i];
             }
-
             return sum;
+        }
+
+        /**
+         * Para poder utilizar o 'a' por referencia, basta adicionar o ref no método como o
+         * exemplo abaixo. Fazendo assim, a variavel é obtida por referencia e não por valor.
+         * Sendo assim, a variavel 'a' no programa principal sera alterado quando o método
+         * Triple for executado.
+         */
+        public static void Triple(ref int x) {
+            x = x * 3;
+        }
+
+        /**
+         * O modificador out é similar ao ref (faz o parametro ser uma referencia para a variavel original),
+         * mas não exige que a variavel original seja iniciada.
+         */
+        public static void TripleSecond(int origin, out int result) {
+            result = origin * 3;
         }
     }
 }
--- ws-rider/Params/Params/Program.cs
+++ ws-rider/Params/Params/Program.cs
@@ -4,8 +4,20 @@ namespace Params {
     class Program {
         static void Main(string[] args) {
             int result = Calculator.Sum(10, 20, 30, 40);
-
             Console.WriteLine(result);
+
+            // Modificador de parâmetros REF
+            int a = 10;
+            Calculator.Triple(ref a); // Para passar o 'a' como referência.
+            Console.WriteLine(a);
+
+            // Modificador de parâmetros OUT
+            int triple;
+            Calculator.TripleSecond(a, out triple);
+            Console.WriteLine(a);
+            Console.WriteLine(triple);
+            
+            // Ambos (ref e out) são considerados "code smells" (design ruim) e devem ser evitados.
         }
     }
 }
