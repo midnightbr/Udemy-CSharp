45e2973be7b1f33095fcd31ca9ba0120fc68f353
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-01-05 22:41:20 -0300

    Foreach Introdução e demonstração do laço de repetição foreach. Foi usado um vetor para poder demonstrar seu funcionamento e sintaxe.

---
+++ ws-rider/Foreach/Foreach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Foreach {
+    class Program {
+        static void Main(string[] args) {
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+            
+            // Método com for para percorrer o vetor
+            for (int i = 0; i < vect.Length; i++) {
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine("-------------------");
+
+            // Método com o foreach para percorrer o vetor
+            foreach (string obj in vect) {
+                Console.WriteLine(obj);
+            }
+            
+        }
+    }
+}
