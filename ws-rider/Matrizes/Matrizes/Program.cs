da50e1aa234bffb6bd845d0b436cc3a19c32324a
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-01-07 12:57:01 -0300

    Matrizes Introdução a matrizes e algumas funcionalidades, além de usar alguns métodos para poder ter algumas informações sobre a matriz criada.

---
+++ ws-rider/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Matrizes {
+    class Program {
+        static void Main(string[] args) {
+            // Criando uma matriz
+            double[,] mat = new double[2, 3];
+
+            // .lenght mostra o tamanho da matriz
+            Console.WriteLine(mat.Length);
+            
+            // .Rank mostra a quantidade de linhas que a matriz tem
+            Console.WriteLine(mat.Rank);
+            
+            // .GetLenght mostra o tamanho das dimensões
+            // Dimensão 0 = tamanho em questão de linhas
+            Console.WriteLine(mat.GetLength(0));
+            // Dimensão 1 = tamanho em questão de colunas
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
