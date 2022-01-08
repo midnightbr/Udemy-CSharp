fa357e42a6c8aaa26dce464b588e4e0c633c83fd
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-01-08 17:51:48 -0300

    Exercício 003 Exercício demonstrando a utilização de matrizes e como adicionar valores nela, além de percorrer seus valores guardados.

---
+++ ws-rider/Exercicio003/Exercicio003/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace Exercicio003 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite o tamanho da matriz: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os valores separados por espaço: ");
+            // string[] vetor = Console.ReadLine().Split(' ');
+            int[,] matriz = new int[tamanho, tamanho];
+            for (int x = 0; x < tamanho; x++) {
+                string[] vetor = Console.ReadLine().Split(' ');
+                for (int y = 0; y < tamanho; y++) {
+                    matriz[x, y] = int.Parse(vetor[y]);
+                }
+            }
+
+            Console.WriteLine("Main diagonal:");
+
+            for (int x = 0; x < tamanho; x++) {
+                for (int y = 0; y < tamanho; y++) {
+                    if (x == y) {
+                        Console.Write(matriz[x, y] + " ");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            int negative = 0;
+            foreach (int obj in matriz) {
+                if (obj < 0) {
+                    negative++;
+                }
+            }
+
+            Console.WriteLine("Negative numbers = " + negative);
+            
+        }
+    }
+}
