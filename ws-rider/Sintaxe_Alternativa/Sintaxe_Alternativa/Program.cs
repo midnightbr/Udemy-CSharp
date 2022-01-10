5f1c9a7356fc9a87b7fb268178c20f38566530ac
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-01-10 12:25:16 -0300

    Sintaxe Alternativa Mostrado uma síntaxe mais simples e direta para casos em que o a estrutura para tratar o caso em else if seja simples, como no exemplo dado aqui. Assim o código fica mais organizado e simplificado.

---
+++ ws-rider/Sintaxe_Alternativa/Sintaxe_Alternativa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sintaxe_Alternativa {
+    class Program {
+        static void Main(string[] args) {
+            
+            Console.Write("Digite um valor: R$");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            /**
+             * Esse argumento utilizado abaixo, é um teste no qual dependendo do resultado, a variavel vai receber
+             * um valor diferente, substituindo assim a utilização de if e else em cenarios simples como esse,
+             * deixando o código mais limpo e organizado.
+             */
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine("Desconto: " + desconto.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
