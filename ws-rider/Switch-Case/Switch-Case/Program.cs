eeb11e7d482702765f4fba0b88c80d578cd799bd
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-01-09 22:16:51 -0300

    Switch-Case Introdução a estrutura e funcionalidade do switch-case, bastante utilizando quando a necessidade de tomada de decisão, substituindo assim a utilização de vários else if.

---
+++ ws-rider/Switch-Case/Switch-Case/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Switch_Case {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter a number: ");
+            int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (x) {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Wednesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+            }
+
+            Console.WriteLine("Day: " + day);
+        }
+    }
+}
