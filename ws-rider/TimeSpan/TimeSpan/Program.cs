604f50cc0bfd40138c6b17638d67579b53de55f7
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-01-15 12:27:40 -0300

    TimeSpan Estudado a estrutura e a utilização do TimeSpan no C# e suas funcionalidades.

---
+++ ws-rider/TimeSpan/TimeSpan/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TimeSpan {
+    class Program {
+        static void Main(string[] args) {
+            System.TimeSpan time1 = new System.TimeSpan(3, 1, 30);
+            Console.WriteLine(time1);
+            
+            // Construtores do TimeSpan
+            System.TimeSpan time2 = new System.TimeSpan();
+            Console.WriteLine(time2);
+            // Construindo TimeSpan com ticks
+            System.TimeSpan time3 = new System.TimeSpan(900000000L);
+            Console.WriteLine(time3);
+            // Criando um TimeSpan com Dias
+            System.TimeSpan time4 = new System.TimeSpan(1, 2, 11, 50);
+            Console.WriteLine(time4);
+            // Criando um TimeSpan com os milesegundos inclusos
+            System.TimeSpan time5 = new System.TimeSpan(2, 10, 5, 35, 320);
+            Console.WriteLine(time5);
+
+            // Criando um TimeSpan com métodos prontos
+            System.TimeSpan timeDay = System.TimeSpan.FromDays(1.5);
+            Console.WriteLine(timeDay);
+            System.TimeSpan timeHours = System.TimeSpan.FromHours(2.7);
+            Console.WriteLine(timeHours);
+            System.TimeSpan timeMinutes = System.TimeSpan.FromMinutes(1.8);
+            Console.WriteLine(timeMinutes);
+            System.TimeSpan timeSeconds = System.TimeSpan.FromSeconds(2.7);
+            Console.WriteLine(timeSeconds);
+            System.TimeSpan timeMiliseconds = System.TimeSpan.FromMilliseconds(1);
+            Console.WriteLine(timeMiliseconds);
+            System.TimeSpan timeTicks = System.TimeSpan.FromTicks(1250900000L); // Não aceita valores quebrados
+            Console.WriteLine(timeTicks);
+        }
+    }
+}
