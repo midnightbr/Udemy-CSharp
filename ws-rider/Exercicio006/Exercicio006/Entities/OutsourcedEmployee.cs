58e96ff7c1731c94ef9a019c9c081b4e72f2ec15
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-02-11 16:52:11 -0300

    Polimorfismo Exercicio para demonstrar na pratica o funcionamento do polimorfismo, no qual foi declarado os trabalhadores, sendo que se algum for tercerizado, é tratado na condição if no programa principal e sua declaração vai para a subclasse.

---
+++ ws-rider/Exercicio006/Exercicio006/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,17 @@
+namespace Exercicio006.Entities {
+    public class OutsourcedEmployee : Employee {
+
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee() {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour) {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment() {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
---
+++ ws-rider/Exercicio006/Exercicio006/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Exercicio006.Entities;
+
+namespace Exercicio006 {
+    class Program {
+        static void Main(string[] args) {
+            
+            List<Employee> list = new List<Employee>();
+            
+            Console.Write("Enter the number of employees: ");
+            int qtd = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= qtd; i++) {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: $");
+                double valuePerHour = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'y') {
+                    Console.Write("Additional charge: $");
+                    double additionalCharge = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Payments:");
+            foreach (Employee emp in list) {
+                Console.WriteLine($"{emp.Name} - ${emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
