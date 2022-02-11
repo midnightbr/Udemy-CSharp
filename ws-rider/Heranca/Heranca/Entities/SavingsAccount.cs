f60e0e58a3722c6cbf46401b5049f984bf74fca1
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-02-11 09:25:26 -0300

    Classes e Metodos Selados Aqui foi ensinado sobre como selar um metodo, no qual não podera ser sobreposto por uma nova subclasse, assim como, selar uma classe para que ela não possa ser herdada para outra subclasse. Selar classes complexas no qual não existe subclasse, ganha-se desempenho, pois o compilador não ficara analisando o tempo todo a classe para ver se existe uma subclasse da mesma.

--- ws-rider/Heranca/Heranca/Entities/SavingsAccount.cs
+++ ws-rider/Heranca/Heranca/Entities/SavingsAccount.cs
@@ -14,7 +14,13 @@ namespace Heranca.Entities {
             Balance += Balance * InterestRate;
         }
 
-        public override void Withdraw(double amount) {
+        /*
+         * Selando um method para que ele não possa ser utilizado em uma subclasse novamente, ou seja
+         * não pode ser sobreposto para uma nova subclasse. O mesmo pode ser feito com as classes, no qual
+         * basta adicionar a palavra sealed no começo da classe
+         * para que a mesma não seja herdada. Exemplo: sealed class SavingsAccount.
+         */
+        public sealed override void Withdraw(double amount) {
             Balance -= amount;
         }
     }
