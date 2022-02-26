438ea675a1e2f4492f7147723198586cced00899
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-02-25 23:26:54 -0300

    Tratando erros com exceção personalizada

--- ws-rider/ProjectXadrez/ProjectXadrez/Program.cs
+++ ws-rider/ProjectXadrez/ProjectXadrez/Program.cs
@@ -6,16 +6,22 @@ namespace ProjectXadrez {
     class Program {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            
-            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
-            
-            Tela.imprimirTabuleiro(tab);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 0));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
-
+            
         }
     }
 }
---
+++ ws-rider/ProjectXadrez/ProjectXadrez/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    public class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+    }
+}
