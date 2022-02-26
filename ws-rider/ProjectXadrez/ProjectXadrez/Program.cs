d88dc866004c74af4353fbea4726c5d73b926680
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-02-25 22:40:04 -0300

    Começando a colocar as peças no tabuleiro

--- ws-rider/ProjectXadrez/ProjectXadrez/Program.cs
+++ ws-rider/ProjectXadrez/ProjectXadrez/Program.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace ProjectXadrez {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
             
+            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
+            
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
+
         }
     }
 }
