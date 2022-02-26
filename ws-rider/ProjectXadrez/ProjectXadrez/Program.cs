e0f72e640a4a2f08ac1796a39df2598ba98d1e28
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-02-26 12:34:26 -0300

    Criando a classe PosicaoXadrez

--- ws-rider/ProjectXadrez/ProjectXadrez/Program.cs
+++ ws-rider/ProjectXadrez/ProjectXadrez/Program.cs
@@ -9,11 +9,17 @@ namespace ProjectXadrez {
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
-
+                
                 tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 0));
-
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 4));
+                
+                // Testando PosiçãoXadres
+                /*
+                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+                Console.WriteLine(pos);
+                Console.WriteLine(pos.toPosicao()); // Resultando experado é 1, 2
+                */
                 Tela.imprimirTabuleiro(tab);
             }
             catch (TabuleiroException e)
---
+++ ws-rider/ProjectXadrez/ProjectXadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    public class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+        
+        // Converte de Posição no tabuleiro para Posição na matriz
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
