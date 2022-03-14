50bfbe95fda282b38e5bcccb6ebeaa008ffaf02f
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-03-14 12:42:03 -0300

    File and FileInfo Começando a estudar sobre como trabalhar com arquivos em C#. Demonstração simples de File e FileInfo na pratica. OBS.: Trocado SDK do .NET5 para o .NET6

---
+++ ws-rider/FileAndFileInfo/FileAndFileInfo/Program.cs
@@ -0,0 +1,25 @@
+﻿// Caminho do arquivo de origem
+string sourcePath = @"C:\Users\marco\Downloads\Rufus\rufus.log"; 
+// Caminho para o arquivo de destino
+string targetPath = @"C:\Users\marco\Documents\CSharp\File.txt";
+
+try {
+    // Instanciando FileInfo
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    // Copiando o arquivo sourcePath para o arquivo targetPath
+    fileInfo.CopyTo(targetPath);
+    // Mostrando as informações do arquivo por linhas
+    string[] lines = File.ReadAllLines(sourcePath);
+    foreach (string line in lines) {
+        Console.WriteLine(line);
+    }
+}
+catch (IOException e) {
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
+
+
+// // See https://aka.ms/new-console-template for more information
+//
+// Console.WriteLine("Hello, World!");
