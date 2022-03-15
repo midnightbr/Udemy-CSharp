94408cb4c402d1f1499415c9472e374d778565de
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-03-15 14:57:05 -0300

    Stream Writer Utilizado para poder escrever no arquivo ou fazer alterações, como nesse exemplo, onde todas as letras foram para maiusculas no novo arquivos txt.

---
+++ ws-rider/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,19 @@
+﻿string sourcePath = @"C:\Users\marco\Documents\CSharp\FilesTxt\File.txt";
+string targetPath = @"C:\Users\marco\Documents\CSharp\FilesTxt\StreamWriter.txt";
+
+try {
+    // Lendo todo o contéudo dentro do File
+    string[] lines = File.ReadAllLines(sourcePath);
+    
+    // Declarando StreamWriter para escrever no arquivo targetPath
+    using (StreamWriter sw = File.AppendText(targetPath)) {
+        foreach (string line in lines) {
+            sw.WriteLine(line.ToUpper()); // Passando os dados dentro do sourcePath para maiusculo no targetPath
+        }
+    }
+
+}
+catch (IOException e) {
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
