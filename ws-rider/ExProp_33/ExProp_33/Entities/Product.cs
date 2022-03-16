2be1799ab5b80ee6b6d935c8ac87775f4b27bf06
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-03-16 14:13:05 -0300

    Exercício Proposto 33 Fazer um programa para ler o caminho de um arquivo .csv contendo os dados de itens vendidos. Cada item possui um nome, preço unitário e quantidade, separados por vírgula. Você deve gerar um novo arquivo chamado "summary.csv", localizado em uma subpasta chamada "out" a partir da pasta original do arquivo de origem, contendo apenas o nome e o valor total para aquele item (preço unitário multiplicado pela quantidade)

---
+++ ws-rider/ExProp_33/ExProp_33/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace ExProp_33.Entities; 
+
+public class Product {
+    public string Name { get; set; }
+    public double Value { get; set; }
+    public int Qtd { get; set; }
+    
+    public Product(){}
+
+    public Product(string name, double value, int qtd) {
+        Name = name;
+        Value = value;
+        Qtd = qtd;
+    }
+
+    public double Total() {
+        return Value * Qtd;
+    }
+
+    public override string ToString() {
+        return $"{Name} - ${Value} - {Qtd}";
+    }
+}
---
+++ ws-rider/ExProp_33/ExProp_33/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using ExProp_33.Entities;
+
+string sourcePath = @"C:\Users\marco\Documents\CSharp\ws-rider\ExProp_33\ExProp_33\itens.csv";
+string targetPath = @"C:\Users\marco\Documents\CSharp\ws-rider\ExProp_33\ExProp_33\Out\summary.csv";
+
+try {
+    string[] lines = File.ReadAllLines(sourcePath);
+    for (int i = 0; i < lines.Length - 1; i++) {
+        string[] itens = lines[i].Split(";");
+        string name = itens[0];
+        double value = double.Parse(itens[1], CultureInfo.InvariantCulture);
+        int quantity = int.Parse(itens[2]);
+
+        Product product = new Product(name, value, quantity);
+
+        using (StreamWriter sw = File.AppendText(targetPath)) {
+            sw.WriteLine(name + ", $" + product.Total());
+        }
+    }
+
+    using (StreamReader sr = File.OpenText(targetPath)) {
+        while (!sr.EndOfStream) {
+            Console.WriteLine(sr.ReadLine());
+        }
+    }
+    
+}
+catch (IOException e) {
+    Console.WriteLine("Error: " + e.Message);
+}
