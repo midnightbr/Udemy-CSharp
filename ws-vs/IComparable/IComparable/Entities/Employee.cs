d5deb148f479e77c3ddd6391ba62d59896b259ca
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-05-26 23:43:22 -0300

    IComparable Utilizado para quando queremos fazer uma comparação entre objetos, no qual, especificamos qual parte do objeto queremos que seja comparada. No caso desse exemplo, a parte comparada foi o nome.

---
+++ ws-vs/IComparable/IComparable/Entities/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Comparable.Entities
+{
+    internal class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", R$" + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        // Realizando a compração do objeto
+        public int CompareTo(object? obj)
+        {
+            // Verificando se o objeto não é nulo
+            if(!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Employee");
+            }
+
+            // Fazendo downCast de outro funcionario para comparação
+            Employee other = obj as Employee;
+
+            // Indicando qual parte do objeto sera comparada
+            return Name.CompareTo(other.Name); 
+
+            // return Salary.CompareTo(other.Salary); // -> comparação pelo salario e não pelo nome
+        }
+    }
+}
---
+++ ws-vs/IComparable/IComparable/Program.cs
@@ -0,0 +1,27 @@
+﻿// Padrão da linguagem para comparar um objeto a outro
+
+using Comparable.Entities;
+
+string path = @"C:\Users\marco\Documents\CSharp\ws-vs\IComparable\IComparable\text.txt";
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        List<Employee> list = new List<Employee>();
+        while (!sr.EndOfStream)
+        {
+            list.Add(new Employee(sr.ReadLine()));
+        }
+        list.Sort();
+        foreach (Employee emp in list)
+        {
+            Console.WriteLine(emp);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error ocurred:");
+    Console.WriteLine(e.Message);
+}
