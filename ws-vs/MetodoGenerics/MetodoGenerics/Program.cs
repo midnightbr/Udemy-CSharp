232cc30d9930934e09750e5ccc82447128351e79
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-05-28 19:55:57 -0300

    Implementando Generics a um método Nesse exemplo, demonstramos algumas restrições do generics e como contorna uma delas. Aqui aplicamos o Generics a um método específico e não a uma classe. A aula que fala sobre esse assunto: 215.

---
+++ ws-vs/MetodoGenerics/MetodoGenerics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using MetodoGenerics.Entities;
+using MetodoGenerics.Services;
+
+List<Product> list = new List<Product>();
+
+Console.Write("Enter the number of products: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] vect = Console.ReadLine().Split(',');
+    double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+    list.Add(new Product(vect[0], price));
+}
+
+CalculationService calculationService = new CalculationService();
+
+Product p = calculationService.Max(list);
+
+Console.Write("Most expensive: " + p);
---
+++ ws-vs/MetodoGenerics/MetodoGenerics/Services/CalculationService.cs
@@ -0,0 +1,23 @@
+﻿namespace MetodoGenerics.Services;
+
+public class CalculationService
+{
+    public X Max<X>(List<X> list) where X : IComparable
+    {
+        if (list.Count == 0)
+        {
+            throw new ArgumentException("The list can not be empty");
+        }
+        
+        X max = list[0];
+        for (int i = 1; i < list.Count; i++)
+        {
+            if (list[i].CompareTo(max) > 0)
+            {
+                max = list[i];
+            }
+        }
+
+        return max;
+    }
+}
