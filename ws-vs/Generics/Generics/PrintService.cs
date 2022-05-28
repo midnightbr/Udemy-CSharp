b702070391d3f0f9927b5f1e21938de9e880635d
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-05-28 10:14:37 -0300

    Generics Criar um objeto do tipo 'X' no qual, de acordo com o objeto que ele receba, seja string, double, int ou qualquer outro, o objeto receba esse valor e esse tipo, fazendo com que seja necessaria uma classe de código para todos, fazendo assim o reuso do código.

---
+++ ws-vs/Generics/Generics/PrintService.cs
@@ -0,0 +1,42 @@
+﻿namespace Generics;
+
+public class PrintService<X>
+{
+    private X[] _values = new X[10];
+    private int _count = 0;
+
+    public void AddValue(X value)
+    {
+        if (_count >= 10)
+        {
+            throw new InvalidOperationException("PrintService is full");
+        }
+        _values[_count] = value;
+        _count++;
+    }
+
+    public X First()
+    {
+        if (_count == 0)
+        {
+            throw new InvalidOperationException("PrintService is empty");
+        }
+        return _values[0];
+    }
+
+    public void Print()
+    {
+        Console.Write("[");
+        for (int i = 0; i < _count - 1; i++)
+        {
+            Console.Write(_values[i] + ", ");
+        }
+
+        if (_count > 0)
+        {
+            Console.Write(_values[_count - 1]);
+        }
+
+        Console.WriteLine("]");
+    }
+}
---
+++ ws-vs/Generics/Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using Generics;
+
+PrintService<String> printService = new PrintService<String>();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    // int x = int.Parse(Console.ReadLine());
+    string x = Console.ReadLine();
+    printService.AddValue(x);
+}
+
+printService.Print();
+Console.WriteLine("First: " + printService.First());
