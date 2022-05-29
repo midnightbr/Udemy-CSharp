7ed7062c88c0f8e95188e3d6936b0bfac65977af
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-05-29 16:46:37 -0300

    Exercicio Proposto 35 Em um portal de cursos online, cada usuário possui um código único, representado por um número inteiro. Cada instrutor do portal pode ter vários cursos, sendo que um mesmo aluno pode se matricular em quantos cursos quiser. Assim, o número total de alunos de um instrutor não é simplesmente a soma dos alunos de todos os cursos que ele possui, pois pode haver alunos repetidos em mais de um curso. O instrutor Alex possui três cursos A, B e C, e deseja saber seu número total de alunos. Seu programa deve ler os alunos dos cursos A, B e C do instrutor Alex, depois mostrar a quantidade total e alunos dele.

---
+++ ws-vs/ExProp_35/ExProp_35/Entities/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace ExProp_35.Entities
+{
+    internal class Student
+    {
+        public string Course { get; set; }
+        public int StudentCode { get; set; }
+
+        public override int GetHashCode()
+        {
+            return StudentCode.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+            Student other = obj as Student;
+            return StudentCode.Equals(other.StudentCode);
+        }
+    }
+}
---
+++ ws-vs/ExProp_35/ExProp_35/Program.cs
@@ -0,0 +1,36 @@
+﻿using ExProp_35.Entities;
+
+HashSet<Student> students = new HashSet<Student>();
+
+Console.Write("How many students for course A? ");
+int courseA = int.Parse(Console.ReadLine());
+for (int i = 0; i < courseA; i++)
+{
+    Console.Write("Student code: ");
+    int studentCode = int.Parse(Console.ReadLine());
+    students.Add(new Student { Course = "A", StudentCode = studentCode });
+}
+
+Console.Write("How many students for course B? ");
+int courseB = int.Parse(Console.ReadLine());
+for(int i = 0; i < courseB; i++)
+{
+    Console.Write("Student code: ");
+    int studentCode = int.Parse(Console.ReadLine());
+    students.Add(new Student { Course= "B", StudentCode = studentCode });
+}
+
+Console.Write("How many students for course C? ");
+int courseC = int.Parse(Console.ReadLine());
+for (int i = 0; i < courseC; i++)
+{
+    Console.Write("Student code: ");
+    int studentCode = int.Parse(Console.ReadLine());
+    students.Add(new Student { Course = "C", StudentCode = studentCode });
+}
+
+Console.Write("Total students: " + students.Count);
+
+/*
+ * Solução feita pelo professor: https://github.com/acenelio/set2-csharp
+ */
