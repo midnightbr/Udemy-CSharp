d2e3dd7567d4df7a9c7eaa4c9af2fbb8d149727f
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-05-28 21:28:09 -0300

    GetHashCode e Equals Equals verifica se um conteúdo do objeto é igual ao conteúdo de outro objeto, como no IComparable, no entanto ele retorna true ou false na comparação. Já o GetHashCode gera um código de acordo com o objeto, utilizando suas regras internas para a mesma. GetHashCode é mais rápido que o Equals, no entanto menos confiavel que o Equals. O ideial quando se tem muitos dados para comparação é utilizar o GetHashCode para achar algo próximo, e depois utilizar o Equals para verificar os dados.

---
+++ ws-vs/GetHashCode_and_Equals/GetHashCode_and_Equals/Entities/Client.cs
@@ -0,0 +1,25 @@
+﻿namespace GetHashCode_and_Equals.Entities;
+
+public class Client
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    // Mais lento, mais 100% de certeza
+    public override bool Equals(object? obj)
+    {
+        if (!(obj is Client))
+        {
+            return false;
+        }
+        Client other = obj as Client;
+        return Email.Equals(other.Email);
+    }
+
+    // Mais rapido, porém a resposta positiva não é 100% confiavel.
+    // No entanto, é raro haver error nessa comparação
+    public override int GetHashCode()
+    {
+        return Email.GetHashCode();
+    }
+}
---
+++ ws-vs/GetHashCode_and_Equals/GetHashCode_and_Equals/Program.cs
@@ -0,0 +1,9 @@
+﻿using GetHashCode_and_Equals.Entities;
+
+Client a = new Client() { Name = "Maria", Email = "marial@gmail.com" };
+Client b = new Client() { Name = "Alex", Email = "alex@gmail.com" };
+
+Console.WriteLine("Equals? " + a.Equals(b)); // Comparação de conteudo
+Console.WriteLine(a == b); // Compara a referencia do ponteiro de memoria
+Console.WriteLine("GetHashCode A = " + a.GetHashCode());
+Console.WriteLine("GetHashCode B = " + b.GetHashCode());
