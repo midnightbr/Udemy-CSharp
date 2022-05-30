099c2e8c4ecebeb40159c05d8c5fbdc2345cb2d7
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-05-30 01:38:34 -0300

    Criando Extensions de methods já existentes São métodos que estendem a funcionalidade de um tipo, sem precisar alterar o código fonte deste tipo, nem herdar desse tipo • Como fazer um extension method? • Criar uma classe estática • Na classe, criar um método estático • O primeiro parâmetro do método deverá ter o prefixo this, seguido da declaração de um parâmetro do tipo que se deseja estender. Esta será uma referência para o próprio objeto.

---
+++ ws-vs/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,7 @@
+﻿DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+Console.WriteLine(dt.ElapsedTime());
+
+Console.WriteLine("--------------------");
+
+string s1 = "Good morning dear students!";
+Console.WriteLine(s1.Cut(10));
