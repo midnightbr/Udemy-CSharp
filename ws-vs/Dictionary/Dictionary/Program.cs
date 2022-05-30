7f7cde305b2607d6d08e167992fafc781bb83216
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-05-29 22:52:27 -0300

    Dictionary Mostrando na prática a utilização. Se assemelha bastante da função Map de outras linguagens.

---
+++ ws-vs/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+cookies["user"] = "Henrique";
+cookies["email"] = "henrique@gmail.com";
+cookies["phone"] = "99771122";
+
+Console.WriteLine(cookies["phone"]);
+
+cookies.Remove("phone");
+
+Console.WriteLine("Email: " + cookies["email"]);
+
+if (cookies.ContainsKey("phone"))
+{
+    Console.WriteLine("Phone: " + cookies["phone"]);
+}
+else
+{
+    Console.WriteLine("There is not 'phone' key!");
+}
+
+Console.WriteLine("Size: " + cookies.Count);
+
+Console.WriteLine("All Cookies:");
+// KeyValuePair<string, string> pode ser substituido por apenas var.
+foreach (KeyValuePair<string, string> item in cookies)
+{
+    Console.WriteLine(item.Key + "=" + item.Value);
+}
