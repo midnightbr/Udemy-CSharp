c2e0baefcc4545b7e071789a23c7111f84adc955
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-05-30 00:01:05 -0300

    Exercicio Proposto 35 Na contagem de votos de uma eleição, são gerados vários registros de votação contendo o nome do candidato e a quantidade de votos (formato .csv) que ele obteve em uma urna de votação. Você deve fazer um programa para ler os registros de votação a partir de um arquivo, e daí gerar um relatório consolidado com os totais de cada candidato.

---
+++ ws-vs/ExProp_36/ExProp_36/Program.cs
@@ -0,0 +1,38 @@
+﻿Dictionary<string, int> vote = new Dictionary<string, int>();
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(',');
+            string name = line[0];
+            int wishes = int.Parse(line[1]);
+            if (!vote.ContainsKey(name))
+            {
+                vote[name] = wishes;
+            }
+            else
+            {
+                vote[name] += wishes;
+            }
+        }
+
+        foreach (var item in vote)
+        {
+            Console.WriteLine(item.Key + ": " + item.Value);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("Error: " + e.Message);
+}
+
+/**
+ * Solução feita pelo professor: https://github.com/acenelio/dictionary1-csharp
+ */
