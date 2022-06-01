2be9cd6640f0ce042faac093af9b8c49a881da10
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-06-01 02:55:10 -0300

    Introdução  a Delegate

---
+++ ws-vs/Delegate/Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using Course.Services;
+using System;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 20;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
