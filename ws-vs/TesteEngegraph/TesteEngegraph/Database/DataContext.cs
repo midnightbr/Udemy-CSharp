346ee15ae16980bdeb0db5e33dc31ecca3f3f33c
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-06-02 23:31:25 -0300

    Configurado a conexão com o servidor

---
+++ ws-vs/TesteEngegraph/TesteEngegraph/Database/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteEngegraph.Models;
+
+namespace TesteEngegraph.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ContactModel> Contatos { get; set; }
+
+        public DbSet<TypeModel> Tipo { get; set; }
+    }
+}
