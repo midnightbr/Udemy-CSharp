729c0b294f52ab96ccb5316a72dd6526526e79cf
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-06-02 23:16:23 -0300

    Feito o Front-end de cadastro (obs.: falta fazer o filtro da tabela): - Modal Cadastro - Modal Tipo Adicionado Model para Contato e Tipo Adicionado as dependencias para trabalhar com Entity Framework Criado a pasta Database

---
+++ ws-vs/TesteEngegraph/TesteEngegraph/Models/ContactModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TesteEngegraph.Models
+{
+    public class ContactModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Cpf { get; set; }
+        public string Sexo { get; set; }
+        public TypeModel Tipo { get; set; }
+        public Boolean Ativo { get; set; }
+    }
+}
---
+++ ws-vs/TesteEngegraph/TesteEngegraph/Models/TypeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TesteEngegraph.Models
+{
+    public class TypeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
