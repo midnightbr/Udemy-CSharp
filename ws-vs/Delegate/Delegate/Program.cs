475084c29b5098c61db1507bcc82347ed2907be4
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-06-14 12:19:44 -0300

    Predicate Aula mostrando a utilização do predicato em delegates

--- ws-vs/Delegate/Delegate/Program.cs
+++ ws-vs/Delegate/Delegate/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Course.Services;
+using Delegate.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace Delegate
 {
@@ -23,6 +25,30 @@ namespace Delegate
 
             op.Invoke(a, b);
 
+            // Predicate
+            List<Product> list = new List<Product>();
+            // Adicionando produtos na lista
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 35.00));
+            list.Add(new Product("Monitor", 1300.00));
+            list.Add(new Product("Memoria", 80.25));
+
+            // Removendo os produtos da lista que tenham um valor
+            // maior que R$100.00
+            list.RemoveAll(ProductTeste);
+
+            // Imprimindo a lista no console
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+        // Testando se o valor do produto é maior ou igual a R$100
+        public static bool ProductTeste(Product product)
+        {
+            return product.Price >= 100.00;
         }
     }
 }
