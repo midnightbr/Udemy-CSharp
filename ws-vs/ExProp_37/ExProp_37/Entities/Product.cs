94ead5b7fc2aa218a7bf39bdbe2f599a14811d0e
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-07-02 15:08:51 -0300

    Exercicio Proposto 37 Fazer um programa para ler um conjunto de produtos a partir de um arquivo em formato .csv (suponha que exista pelo menos um produto). Em seguida mostrar o preço médio dos produtos. Depois, mostrar os nomes, em ordem decrescente, dos produtos que possuem preço inferior ao preço médio.

---
+++ ws-vs/ExProp_37/ExProp_37/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ExProp_37.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
